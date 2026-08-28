877501fe0225668d62aab637223753adf66bab5a
drozdovapb <drozdovapb@gmail.com>
2026-08-28 13:04:24 +0800

    clean up Supplementary for R1

---
+++ Supplementary_material/code.sh
@@ -0,0 +1,9 @@
+pdftk ../Fig1_delimitation_discordance/FigS1_wcapt.pdf \
+	../Fig1_delimitation_discordance/FigS2_delimitation_tree_wcaption.pdf \
+	../Fig1_delimitation_discordance/FigS3_disc_tree_more_colors_wbars_wcapt.pdf \
+ 	../Fig6_patristic_distances/FigS4.pdf \
+cat output Figures_S1-S4.pdf
+
+
+
+
